66f4640b25b3533a7964df327259c092b73b50d7
AbdulHannan <abdul.hannan@ideofuzion.com>
2024-11-20 13:35:32 +0500

    push to develop

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './core/interceptor/response-interceptor';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,12 +14,22 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ResponseInterceptor());
 
   // Swagger configuration
+  app.use(
+    '*/api',
+    basicAuth({
+      users: {
+        admin: 'admin',
+      },
+      challenge: true,
+    }),
+  );
   const swaggerConfig = new DocumentBuilder()
     .setTitle('User API')
     .setDescription('User Auth API')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
+
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
